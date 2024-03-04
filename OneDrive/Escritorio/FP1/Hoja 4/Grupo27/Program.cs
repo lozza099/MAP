02c736fdee95e193c17f0af2a884a2f3b3414c42
lozza099 <158841979+lozza099@users.noreply.github.com>
2024-03-04 11:34:44 +0100

    Archivos inciales

---
+++ OneDrive/Escritorio/FP1/Hoja 4/Grupo27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Timers;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dime un número y te daré su representación en binario. ");
+            int num = int.Parse(Console.ReadLine());
+            string binario = "";
+            while (num > 0)
+            {
+                int bit = num % 2;
+                binario = bit + binario;
+                num /= 2;
+            }
+            Console.WriteLine("El número en binario es "+ binario);
+
+        }
+    }
+}
